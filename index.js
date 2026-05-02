79dc7271bdd6fdb40d0894907f0f0330c8f8cf8c
Your Name <vasipallieshan@gmail.com>
2026-05-02 20:40:17 +0800

    dropped starkloud login support due to difference in auth logic

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.use(cookieParser());
 const supabase = createClient(process.env.supaURL, process.env.supaKEY);
 
